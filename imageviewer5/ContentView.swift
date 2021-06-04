c5ed041cd0453a596e5627803d4ff642f6318069
djs <lambdan@users.noreply.github.com>
2021-06-05 01:41:25 +0200

    support drag n drop onto window

--- imageviewer5/ContentView.swift
+++ imageviewer5/ContentView.swift
@@ -3,13 +3,40 @@ import AppKit
 import Combine
 import Foundation
 
-var screen_width = NSScreen.main?.frame.width // Current display max width
-var screen_height = NSScreen.main?.frame.height // and height
+//var screen_width = NSScreen.main?.frame.width // Current display max width
+//var screen_height = NSScreen.main?.frame.height // and height
 
-struct ContentView: View {
+
+struct ContentView: View, DropDelegate {
+    
+    // TODO should also implement validateDrop function to validate file extension
+    func performDrop(info: DropInfo) -> Bool {
+        guard let itemProvider = info.itemProviders(for: [(kUTTypeFileURL as String)]).first else { return false }
+        itemProvider.loadItem(forTypeIdentifier: (kUTTypeFileURL as String), options: nil) { (data,error) in
+            let url = URL(dataRepresentation: data as! Data, relativeTo: nil)!
+            if HandledFileExtensions.contains(url.pathExtension) {
+                DispatchQueue.main.async {
+                    set_new_url(in_url: url.absoluteString)
+                    send_NC(text: "Image drag n dropped")
+                }
+            }
+        }
+        return true
+    }
+    
+    func dropEntered(info: DropInfo) {
+        //self.bgcolor = Color(.systemOrange)
+    }
+    
+    func dropExited(info: DropInfo) {
+        print("drop exited")
+        //self.bgcolor = Color(.systemBlue)
+    }
+    
     @State var url_string = get_URL_String()
     @State var imgW = CGFloat(get_IMG_Size(axis: "Width"))
     @State var imgH = CGFloat(get_IMG_Size(axis: "Height"))
+    //@State var bgcolor = Color(.systemBlue)
     
     
     let pub = NotificationCenter.default.publisher(for: NSNotification.Name(NCName))
@@ -18,25 +45,26 @@ struct ContentView: View {
         VStack {
             if url_string == "" {
                 Text("No image loaded").fixedSize().padding(50)
+                    
                 
             } else {
                 Image(nsImage: NSImage(contentsOf: URL(string:url_string)!)!).resizable().aspectRatio(contentMode: .fit)
             }
             
-            }.onReceive(pub) {_ in
-            // This gets run when notification is sent
-            self.url_string = get_URL_String()
-            self.imgW = CGFloat(get_IMG_Size(axis: "Width"))
-            self.imgH = CGFloat(get_IMG_Size(axis: "Height"))
+            
+            }
+            .onReceive(pub) {_ in
+                // This gets run when notification is sent
+                self.url_string = get_URL_String()
+                self.imgW = CGFloat(get_IMG_Size(axis: "Width"))
+                self.imgH = CGFloat(get_IMG_Size(axis: "Height"))
             
         }
+        
+        
         .frame(minWidth: 400, idealWidth: imgW, maxWidth: NSScreen.main?.frame.height, minHeight: 300, idealHeight: imgH, maxHeight: NSScreen.main?.frame.height)
+        //.background(Rectangle().fill(bgcolor))
+        .onDrop(of: [(kUTTypeFileURL as String)], delegate: self)
     }
 }
-/*
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}*/
 
